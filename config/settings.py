7adf48cc94f7e1f7f10acfe5f4b90c6a0c534fa7
Osorio Moreira Hornay <omhornay@tic.gov.tl>
2026-08-21 17:18:37 +0900

    Fix STATIC_ROOT for Render

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -205,4 +211,13 @@ SIMPLE_JWT = {
         days=1
     ),
 
+}
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
